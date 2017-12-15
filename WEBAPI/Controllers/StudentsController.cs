40fa0c03cd0edc6f9f521ed6ebfd78635268874f
Manuel <manuepeva@gmail.com>
2017-12-14 19:40:10 -0500

    route attribute asp.net web api

---
+++ WEBAPI/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEBAPI.Models;
+
+namespace WEBAPI.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        static List<Student> students = new List<Student>()
+        {
+            new Student() {Id = 1, Name = "tome"},
+            new Student() {id = 2, Name= "dos"},
+            new Student() {id  3, Name  "tres"}
+        };
+
+        public static List<Student> Students { get => students; set => students = value; }
+
+        public IEnumerable<Student> Get()
+        {
+            return Students;
+        }
+
+        public Student Get(int id)
+        {
+            return Students.FirstOrDefault(s => s.Id == id);
+        }
+
+        public IEnumerable<string> GetStudentCourses(int id)
+        {
+            if (id == 1)
+                return new List<String>() { "C#", "ASP.NET", "SQL-Server" };
+            else if (id == 2)
+                return new List<string>() { "Phyton", "Mangalore", "Oracle-Server" };
+            else
+                return new List<string>() { "Anything", "bootstrap", "SQL-WorkBench };     
+        };
+    }
+}
