3ba057c6805908b80de4614566c6cd857c34e84e
Manuel <manuepeva@gmail.com>
2017-12-16 10:16:09 -0500

    httpResponseMessage vs IhttpActionResult

--- WEBAPI/Controllers/StudentsController.cs
+++ WEBAPI/Controllers/StudentsController.cs
@@ -8,48 +8,30 @@ using WEBAPI.Models;
 
 namespace WEBAPI.Controllers
 {
-    [RoutePrefix("api/students")]
+    
     public class StudentsController : ApiController
     {
         static List<Student> students = new List<Student>()
         {
             new Student() {Id = 1, Name = "Jose"},
-            new Student() {Id = 1, Name = "Lucho"},
-            new Student() {Id = 1, Name = "Riki"},
+            new Student() {Id = 2, Name = "Lucho"},
+            new Student() {Id = 3, Name = "Riki"},
         };
-        [Route("~/api/teachers")]
-        public IEnumerable<Teacher> GetTeachers()
+
+        public IHttpActionResult Get()
         {
-            List<Teacher> teachers = new List<Teacher>()
+            return Ok(students);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
             {
-                new Teacher(){ Id=1, Name = "Rob" },
-                new Teacher(){ Id=2, Name = "Robin" },
-                new Teacher(){ Id=3, Name = "Rick" }
-            };
-
-            return teachers;
-        }
-
-        [Route("")]
-        public IEnumerable<Student> Get()
-        {
-            return students;
-        }
-        [Route("{id}")]
-        public Student Get(int id)
-        {
-            return students.FirstOrDefault(s => s.Id == id);
-        }
-
-        [Route("{id}/courses")]
-        public IEnumerable<string> GetStudentCourses(int id)
-        {
-            if (id == 1)
-                return new List<String>() { "C#", "ASP.NET", "SQL-Server" };
-            else if (id == 2)
-                return new List<String>() { "Phyton", "Mangalore", "Oracle-Server" };
-            else
-                return new List<String>() { "Anything", "bootstrap", "SQL-WorkBench" };
+                return Content(HttpStatusCode.NotFound, "student not founded");
+            }
+            return Ok(student);
         }
     }
+          
 }
