fc8a6b404935d364ec1e1c7cded95f12d2528828
ManuDeveloper <manuepeva@gmail.com>
2020-08-06 11:08:40 -0500

    Updates

---
+++ precache-manifest.cbbe956b3c234f71fd0d1df89ed86992.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5df7145b8f5055cd7ed762c73781a24",
+    "url": "/API---Breaking-Bad-Series-/index.html"
+  },
+  {
+    "revision": "dbfdf19f72e620cdb59f",
+    "url": "/API---Breaking-Bad-Series-/static/css/main.768a7b26.chunk.css"
+  },
+  {
+    "revision": "fb81d6809e763c107449",
+    "url": "/API---Breaking-Bad-Series-/static/js/2.05debbc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/API---Breaking-Bad-Series-/static/js/2.05debbc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfdf19f72e620cdb59f",
+    "url": "/API---Breaking-Bad-Series-/static/js/main.9e96fa1e.chunk.js"
+  },
+  {
+    "revision": "2e6f751f6de7474005d4",
+    "url": "/API---Breaking-Bad-Series-/static/js/runtime-main.bc135282.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/API---Breaking-Bad-Series-/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/API---Breaking-Bad-Series-/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/API---Breaking-Bad-Series-/static/media/spinner.235b9b3e.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2499f7b165023558879932ab0ca43031.js"
+  "/API---Breaking-Bad-Series-/precache-manifest.cbbe956b3c234f71fd0d1df89ed86992.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/API---Breaking-Bad-Series-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
